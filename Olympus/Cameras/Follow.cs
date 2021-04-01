680f6a3f099b616eb12407ef83153c6adef3b16b
PumpShotty64 <46170841+jordanguyent@users.noreply.github.com>
2021-04-01 12:49:45 -0700

    Updated Project Settings and Camera Zoom
    Made it so that Camera Zoom is always scalled to 1. Makes it easier to create static levels

--- Olympus/Cameras/Follow.cs
+++ Olympus/Cameras/Follow.cs
@@ -4,7 +4,7 @@ using System;
 public class Follow : Camera2D
 {
 	// Camera Constants
-	private float STANDARDSCALE = 1.25f;
+	private float STANDARDSCALE = 1.00f;
 	private int STANDARDLIMIT = 10000000;
 	// limits here for boundaries
 	
