f81ec0af31c005a5af7569d110417af67dd8daae
saltypond <tealake@tuta.io>
2021-04-14 02:52:06 -0700

    Added config to read from. Cleaned up scenehandler

---
+++ Olympus/Non-Levels/Config.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class Config : Node
+{
+    public Godot.Collections.Array PlayerSpawnPosition = null;
+
+    public void SetPlayerSpawn(Godot.Collections.Array SpawnLocation){
+        this.PlayerSpawnPosition = SpawnLocation;
+    }
+}
--- Olympus/Non-Levels/SceneHandler.cs
+++ Olympus/Non-Levels/SceneHandler.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 
 public class SceneHandler : Node
 {
+	[Signal]
+	public delegate void SetPlayerSpawn(Godot.Collections.Array StagePos);
+
 	Godot.Collections.Dictionary<string, string> stageList;
 	public Node CurrentScene{get; set;}
 	private string CurrentPath;
@@ -24,55 +27,67 @@ public class SceneHandler : Node
 		GetTree().SetCurrentScene(CurrentScene);
 	}
 
+	public Godot.Collections.Dictionary<string, object> JSONToDict(string Path){
+		Godot.File PathFile = new Godot.File();
+		PathFile.Open(Path, Godot.File.ModeFlags.Read);
+		var Data = new Godot.Collections.Dictionary<string, object>( (Godot.Collections.Dictionary) JSON.Parse(PathFile.GetLine()).Result );
+		PathFile.Close();
+		return Data;
+	}
+
+	public override void _Ready(){
+		Config CONFIG = (Config)GetNode("/root/Config");
+		if (CONFIG == null)
+		{
+			throw new ArgumentNullException("Edwin: Autoload Config not found"); 
+		}
+		Connect("SetPlayerSpawn",CONFIG,"SetPlayerSpawn");
+	}
+
 	public void ChangeStage(string ZoneEntered){
 		//Get which stage to load next
-		Godot.File ZoneFile = new Godot.File();
 		string ZonePath = $"res://Levels/Level{this.CurrentLevel}/Level{this.CurrentLevel}Zone.json";
-		ZoneFile.Open(ZonePath, Godot.File.ModeFlags.Read);
-		var ZoneData = new Godot.Collections.Dictionary<string, object>( (Godot.Collections.Dictionary)JSON.Parse(ZoneFile.GetLine()).Result );
-		ZoneFile.Close();
-		//Get where to load in that stage
-		Godot.File SpawnFile = new Godot.File();
-		string SpawnPath = $"res://Levels/Level{this.CurrentLevel}/Level{this.CurrentLevel}Spawn.json";
-		SpawnFile.Open(SpawnPath, Godot.File.ModeFlags.Read);
-		var SpawnData = new Godot.Collections.Dictionary<string, object>( (Godot.Collections.Dictionary)JSON.Parse(SpawnFile.GetLine()).Result );
-		SpawnFile.Close();
-
+		var ZoneData = JSONToDict(ZonePath);
 		var StageData = (Godot.Collections.Dictionary)ZoneData[$"{CurrentStage}"];
 		string NextLevel = (string)StageData[ZoneEntered];
+
+		//Get where to load in that stage
+		string SpawnPath = $"res://Levels/Level{this.CurrentLevel}/Level{this.CurrentLevel}Spawn.json";
+		var SpawnData = JSONToDict(SpawnPath);
+
 		//Use the term "NextWorld" in the json to indicate moving from level 1 to level 2 as an example
 		//NOTE: LEVELS =/= STAGES
 		if(NextLevel == "NextWorld"){
+
+		}else
+		if(NextLevel == "PreviousWorld"){
 
 		}else{
 			//Stores the variables used for checking spawn point
 			int FromLevel = CurrentStage;
 			int ToLevel;
-			//Loads new scene
-			string CurrentStageScenePath = $"res://Levels/Level{this.CurrentLevel}/Stages/Stage{NextLevel}.tscn";
+			//Gets the string for the next scene since we will modify some values
+			string NextStagePath = $"res://Levels/Level{this.CurrentLevel}/Stages/Stage{NextLevel}.tscn";
 			//Loads in current save data
-			File LoadSaveFile = new Godot.File();
-			LoadSaveFile.Open(this.SavePath, Godot.File.ModeFlags.Read);
-			//GD.Print(LoadSaveFile.IsOpen());
-			var SaveData = new Godot.Collections.Dictionary<string, object>( (Godot.Collections.Dictionary)JSON.Parse(LoadSaveFile.GetLine()).Result );
-			LoadSaveFile.Close();
-			//Modifying the data
+			var SaveData = JSONToDict(this.SavePath);
+			//Modifying the data in the SceneHandler
 			CurrentStage = int.Parse(NextLevel);
 			ToLevel = CurrentStage;
-			Godot.Collections.Array NewStagePosition = new Godot.Collections.Array(){CurrentLevel, int.Parse(NextLevel)};
-			SaveData["WorldData"] = NewStagePosition; 
+			SaveData["WorldData"] = new Godot.Collections.Array(){CurrentLevel, int.Parse(NextLevel)}; 
+			
 			//Overwrites save data - alternatively we can just save if the user presses exit from a menu
 			//TODO: add a menu that prompts settings when the user presses ESC while in game
 			File UpdateSaveFile = new Godot.File();
 			UpdateSaveFile.Open(this.SavePath, Godot.File.ModeFlags.Write);
 			UpdateSaveFile.StoreString(JSON.Print(SaveData));
 			UpdateSaveFile.Close();
-			//Set player position
-			var StagePositionData = (Godot.Collections.Dictionary)SpawnData[$"{ToLevel}"];
-			this.PlayerSpawnPosition = (Godot.Collections.Array)StagePositionData[$"{FromLevel}"];
-			// GD.Print(this.PlayerSpawnPosition);
 
-			SceneHelper(CurrentStageScenePath);
+			//Config fie will handle
+			var StagePositionData = (Godot.Collections.Dictionary)SpawnData[$"{ToLevel}"];
+			EmitSignal("SetPlayerSpawn", (Godot.Collections.Array)StagePositionData[$"{FromLevel}"]);
+
+			//Load next level :)
+			SceneHelper(NextStagePath);
 		}
 	}
 
@@ -88,9 +103,11 @@ public class SceneHandler : Node
 		//Setting pause menu to ON so we can pause in game but not in menu
 		Pause PauseMenu = (Pause)GetNode("/root/Pause");
 		PauseMenu.On = true;
-		// //TODO: Deal with gameplay options here - this is a stub
 
+		// //TODO: Deal with gameplay options here - this is a stub
 		// //TODO: Deal with player unlocked abilities here - this is a stub
+		// This is already opened so don't close it since we're not done with it
+		EmitSignal(SaveFile)
 
 		string CurrentStageScenePath = $"res://Levels/Level{this.CurrentLevel}/Stages/Stage{this.CurrentStage}.tscn";
 		SceneHelper(CurrentStageScenePath);
